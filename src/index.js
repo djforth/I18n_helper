07d97f814c06389ab0a987a2e12b6b3f7093dc7a
Adrian Stainforth <adrian.stainforth@gmail.com>
2018-02-22 11:37:43 +0000

    Upgrade to rollup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+export const manageTranslations = (root = 'react') => section => (key, options = {}) => {
+  if (!I18n) return options.defaultValue ? options.defaultValue : '';
+  let t_key = `${root}.${section}`;
+  if (key) {
+    t_key += `.${key}`;
+  }
 
-export const manageTranslations = (root = 'react')=>(section)=>{
-  return (key, options = {})=>{
-    if (!I18n) return (options.defaultValue) ? options.defaultValue : '';
-    let t_key = `${root}.${section}`;
-    if (key){
-      t_key += `.${key}`;
-    }
-
-    return I18n.t(t_key, options);
-  };
+  return I18n.t(t_key, options);
 };
 
-export const actionsTranslation = (item)=>{
+export const actionsTranslation = item => {
   let t = manageTranslations()('action_btns');
   let type = manageTranslations()(item)('title');
 
-  return t('add', {type});
+  return t('add', { type });
 };
